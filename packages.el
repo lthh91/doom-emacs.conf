fb1a05eb73ba83eb737156f38877dda2c8708af8
Huy Mai <huy.mai@ericsson.com>
2020-06-30 10:21:46 +0300

    Add groovy and ttcn-el packages

--- packages.el
+++ packages.el
@@ -10,12 +10,17 @@
 ;(package! some-package)
 (package! ox-hugo)
 
+;;groovy
+(package! groovy-mode)
+
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/raxod502/straight.el#the-recipe-format
 ;(package! another-package
 ;  :recipe (:host github :repo "username/repo"))
 
+(package! ttcn-el
+  :recipe (:host github :repo "dholm/ttcn-el"))
 ;; If the package you are trying to install does not contain a PACKAGENAME.el
 ;; file, or is located in a subdirectory of the repo, you'll need to specify
 ;; `:files' in the `:recipe':
